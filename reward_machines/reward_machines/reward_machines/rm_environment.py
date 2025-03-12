f2890dfb5c11578d6fad5d433f63534e36a9dbaf
xdvisch <xander.devisch@ugent.be>
2025-03-12 13:41:57 +0100

    changed the amount of steps to try out

--- reward_machines/reward_machines/reward_machines/rm_environment.py
+++ reward_machines/reward_machines/reward_machines/rm_environment.py
@@ -77,7 +77,7 @@ class RewardMachineEnv(gym.Wrapper):
         self.current_u_id  = self.current_rm.reset()
         self.steps_in_current_u = 0
         self.previous_u_id = self.current_u_id
-        self.max_steps_in_u_id = [20, 20, 20, 40]
+        self.max_steps_in_u_id = [15, 15, 20, 40]
 
         # Adding the RM state to the observation
         return self.get_observation(self.obs, self.current_rm_id, self.current_u_id, False)
