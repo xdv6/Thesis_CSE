535308e02e2651e64780cb060baae2eafaf2f80e
xdvisch <xander.devisch@ugent.be>
2025-04-09 13:26:21 +0200

    fixed finalization problem

--- reward_machines/reward_machines/rl_agents/dhrm/dhrm.py
+++ reward_machines/reward_machines/rl_agents/dhrm/dhrm.py
@@ -208,7 +208,7 @@ def learn(env,
                     if print_freq is not None:
                         logger.log("Saving model due to mean reward increase: {} -> {}".format(
                                    saved_mean_reward, mean_100ep_reward))
-                    save_variables(model_file)
+                        save_variables(model_file)
                     model_saved = True
                     saved_mean_reward = mean_100ep_reward
         if model_saved:
